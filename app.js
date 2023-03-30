1fdeafc7a5f3950ef785e662a5b80dccc20f118d
A00832425 <113365949+A00832425@users.noreply.github.com>
2023-03-29 19:45:39 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const candidatesRoute = require("./routes/candidates");
+const projectRoute = require("./routes/project");
+const projectUser = require("./routes/user");
+const projectMatches = require("./routes/match");
+const projectDem = require("./routes/demand");
 var cors = require("cors");
 
 dotenv.config();
@@ -15,6 +19,14 @@ app.get("/", function (req, res) {
 });
 
 app.use("/api/candidates", candidatesRoute);
+
+app.use("/api/project", projectRoute);
+
+app.use("/api/user", projectUser);
+
+app.use("/api/matches", projectMatches);
+
+app.use("/api/demand", projectDem);
 
 app.listen(port, function () {
   console.log(`Servidor en el puerto ${port} `);
