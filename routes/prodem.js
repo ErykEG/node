6d7006d1885970abce235d6f3da2145875853a23
A00832425 <113365949+A00832425@users.noreply.github.com>
2023-04-10 17:26:28 -0600

    nueva tabla

---
+++ routes/prodem.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const sql = require("mssql");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  await sql.connect(process.env.DB_CONNECTION);
+
+  const result =
+    await sql.query`SELECT * FROM Projects_Demand`;
+
+  res.json(result.recordset);
+});
+
+router.get("/test", (req, res) => {
+  res.json({ test: "test" });
+});
+
+router.post("/test", (req, res) => {
+  res.json({ test: "test" });
+});
+
+module.exports = router;
