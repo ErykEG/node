69f2c0b3e3e20c09079830dd1eb13a435fc7a6fc
A00832425 <113365949+A00832425@users.noreply.github.com>
2023-04-12 16:55:35 -0600

    Update match.js

--- routes/match.js
+++ routes/match.js
@@ -15,20 +15,3 @@ router.post("/add", async (req, res) => {
     res.status(500).send("Server error");
   }
 });
-router.post("/add", async (req, res) => {
-  try {
-    const pool = await sql.connect(process.env.DB_CONNECTION);
-
-    const result = await pool
-      .request()
-      .input("title", sql.VarChar, req.body.title)
-      .query(
-        "INSERT INTO Profiles (title) VALUES (@title)"
-      );
-
-    res.send("Expense added successfully.");
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Server error");
-  }
-});
