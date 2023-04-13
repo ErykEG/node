e72b467da4cf57ccfc4f1dc9e3fb7ddb0d961197
A00832425 <113365949+A00832425@users.noreply.github.com>
2023-04-13 13:30:29 -0600

    Update match.js

--- routes/match.js
+++ routes/match.js
@@ -6,10 +6,27 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   await sql.connect(process.env.DB_CONNECTION);
 
-  const result =
-    await sql.query`SELECT * FROM Demand_Stack`;
+  const result = await sql.query`select * from Profiles`;
 
   res.json(result.recordset);
+});
+
+router.post("/add", async (req, res) => {
+  try {
+    const pool = await sql.connect(process.env.DB_CONNECTION);
+
+    const result = await pool
+      .request()
+      .input("title", sql.VarChar, req.body.title)
+      .query(
+        "INSERT INTO Profiles (title) VALUES (@title)"
+      );
+
+    res.send("Expense added successfully.");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 });
 
 router.get("/test", (req, res) => {
