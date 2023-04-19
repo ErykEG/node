9a411811f5e6199e0d4948f0e2b6a37e97403488
ErykEG <erykegonzalez@gmail.com>
2023-04-19 16:25:40 -0600

    Update match.js

--- routes/match.js
+++ routes/match.js
@@ -3,12 +3,57 @@ const sql = require("mssql");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/gc", async (req, res) => {
   await sql.connect(process.env.DB_CONNECTION);
 
-  const result = await sql.query`select * from Profiles`;
+  const result = await sql.query`SELECT COLUMN_NAME 
+    FROM INFORMATION_SCHEMA.COLUMNS 
+    WHERE TABLE_NAME = 'Candidates';`;
 
   res.json(result.recordset);
+});
+
+router.post("/q1", async (req, res) => {
+  try {
+    const variable2 = req.body.variable2;
+
+    console.log(variable2);
+
+    const pool = await sql.connect(process.env.DB_CONNECTION);
+
+    const result = await pool
+      .request()
+      .input("variable", sql.NVarChar(300), variable2)
+      .query(
+        `SELECT * FROM Candidates WHERE Name_Candidates like '%'${variable2}`
+      );
+
+    console.log(result.recordset);
+    res.send(result.recordset);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while processing your request");
+  }
+});
+
+router.post("/q2", async (req, res) => {
+  try {
+    const variable = req.body.variable;
+
+    console.log(variable);
+
+    const pool = await sql.connect(process.env.DB_CONNECTION);
+
+    const result = await pool
+      .request()
+      .input("variable", sql.NVarChar(100), variable)
+      .query(`SELECT DISTINCT ${variable} FROM Candidates`);
+
+    res.send(result.recordset);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while processing your request");
+  }
 });
 
 router.post("/add", async (req, res) => {
@@ -20,9 +65,9 @@ router.post("/add", async (req, res) => {
       .input("title", sql.VarChar, req.body.title)
       .input("amount", sql.Decimal, req.body.amount)
       .query(
-        "INSERT INTO [dbo].[Profiles] ([Id_Profiles], [Name_Profiles], [Email_Profiles], [Permit_Profiles]) VALUES (50, 'nombre', 'correo', 3) GO"
+        "INSERT INTO [dbo].[Profiles] ([Id_Profiles], [Name_Profiles], [Email_Profiles], [Permit_Profiles]) VALUES (@amount, @title, 'correo', 3)"
       );
-
+    console.log(result);
     res.send("Expense added successfully.");
   } catch (err) {
     console.error(err);
